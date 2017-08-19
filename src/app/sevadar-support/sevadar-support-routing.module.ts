4bfe3882554644f9bf6cdd4c91cac451fdb06b43
Parjinder Pannu <ppannu@cisco.com>
2017-08-19 09:04:59 -0700

    Made some changes to get routing working for module sevadar-support

--- src/app/sevadar-support/sevadar-support-routing.module.ts
+++ src/app/sevadar-support/sevadar-support-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SevadarSupportComponent } from "./sevadar-support.component";
+import { FindSevadarComponent } from "./find-sevadar/find-sevadar.component";
 
 const routes: Routes = [
-  // path: 'sevadar-support', Component: SevadarSupportComponent, children: [
-  //   {path}
-  // ]
+  { path: 'sevadar-support', component: SevadarSupportComponent, children: [
+      {path:'', component: FindSevadarComponent}
+  ]}
 ];
 
 @NgModule({
--- src/app/sevadar-support/sevadar-support.module.ts
+++ src/app/sevadar-support/sevadar-support.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { SevadarSupportRoutingModule } from './sevadar-support-routing.module';
 import { SevadarSupportComponent } from './sevadar-support.component';
-import { FindSevadarComponent } from '../src/app/sevadar-support/find-sevadar/find-sevadar.component';
-import { AddSevadarComponent } from '../src/app/sevadar-support/add-sevadar/add-sevadar.component';
+import { FindSevadarComponent } from './find-sevadar/find-sevadar.component';
+import { AddSevadarComponent } from './add-sevadar/add-sevadar.component';
 
 @NgModule({
   imports: [
